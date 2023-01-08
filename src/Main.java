b3ac16b5ba0add9ffe7385d52de3cf6cb51a766e
Ali Timergazin <ya.alit2012@yandex.ru>
2023-01-08 21:12:34 +0300

    f git@github.com:Aliexpress228/JavaTest.git git remote add origin git@github.com:Aliexpress228/JavaTest.git

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+public class Main {
+    public static void main(String[] args) {
+        int maxnum() {
+
+        }
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+
+    }
+}
