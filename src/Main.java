2a1ec1f6a3c2b446a1bb76b423b1916970d3c2b5
Ali Timergazin <ya.alit2012@yandex.ru>
2023-01-08 22:48:23 +0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,19 @@
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections;
 public class Main {
+    public static int maxInList(List array){
+        return (int)Collections.max(array);
+    }
     public static void main(String[] args) {
-        int maxnum() {
+        List<Integer> numbers = new ArrayList<Integer>();
 
-        }
-        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        numbers.add(3);
+        numbers.add(4);
+        numbers.add(7);
+        numbers.add(2);
 
+        System.out.println(numbers);
+        System.out.println("Max in this list is: "+maxInList(numbers));
     }
 }
