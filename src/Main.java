4140b15f97028dcdf1999da5301f298f60786bc4
Ali Timergazin <ya.alit2012@yandex.ru>
2023-01-08 23:45:14 +0300

    loop scanner

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,21 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Collections;
+import java.util.Scanner;
 public class Main {
     public static int maxInList(List array){
         return (int)Collections.max(array);
     }
     public static void main(String[] args) {
         List<Integer> numbers = new ArrayList<Integer>();
+        Scanner sc = new Scanner(System.in);
 
-        numbers.add(3);
-        numbers.add(4);
-        numbers.add(7);
-        numbers.add(2);
-
-        System.out.println(numbers);
+        System.out.println("Write numbers amount: ");
+        int amount = sc.nextInt();
+        for (int i=0;i<amount;i++) {
+            numbers.add(sc.nextInt());
+        }
+        System.out.println("Your list: "+numbers);
         System.out.println("Max in this list is: "+maxInList(numbers));
     }
 }
